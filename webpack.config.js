95438dccb8faeb6e7913754912eed5c60aafab18
TimotheCDGP <timothe.peyregne@efrei.net>
2026-05-04 11:41:54 +0200

    [FEAT] MFE-Cart webpack configuration + event emitter

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'shared': path.resolve(__dirname, '../shared'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfe_cart',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Cart': './src/components/Cart',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        'react-dom': {
+          singleton: true,
+        },
+      },
+    }), ,
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
