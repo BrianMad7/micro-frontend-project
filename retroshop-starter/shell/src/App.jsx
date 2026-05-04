3faae877d057734f048dc65157bee444ddf20c25
Davidc1409 <davidc2509@gmail.com>
2026-05-04 12:29:17 +0200

    Added shell code

--- retroshop-starter/shell/src/App.jsx
+++ retroshop-starter/shell/src/App.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect, Suspense, lazy } from 'react';
-import './App.css';
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import "./App.css";
+import eventBus from "shared/eventBus";
 
+const Product = React.lazy(() => import("mfeProduct/ProductList"));
+const Cart = React.lazy(() => import("mfe_cart/Cart"));
+const Reco = React.lazy(() => import("mfeReco/Reco"));
 // TODO: importer les 3 MFEs avec React.lazy()
 
 function LoadingFallback({ name }) {
@@ -12,6 +16,10 @@ function App() {
 
   useEffect(() => {
     // TODO: ecouter les mises a jour du panier pour le badge
+    const unsub = eventBus.on("cart:add", () => {
+      setCartCount((prev) => prev + 1);
+    });
+    return () => unsub();
   }, []);
 
   return (
@@ -22,17 +30,26 @@ function App() {
       </header>
       <main className="shell-main">
         <section className="product-area">
+          <Suspense fallback={<LoadingFallback name="Products" />}>
+            <Product />
+          </Suspense>
           {/* TODO: afficher mfe-product avec Suspense */}
-          <LoadingFallback name="Products" />
+          {/* <LoadingFallback name="Products" />*/}
         </section>
         <aside className="cart-area">
+          <Suspense fallback={<LoadingFallback name="Cart" />}>
+            <Cart />
+          </Suspense>
           {/* TODO: afficher mfe-cart avec Suspense */}
-          <LoadingFallback name="Cart" />
+          {/* <LoadingFallback name="Cart" />*/}
         </aside>
       </main>
       <section className="reco-area">
+        <Suspense fallback={<LoadingFallback name="Recommendations" />}>
+          <Reco />
+        </Suspense>
         {/* TODO: afficher mfe-reco avec Suspense */}
-        <LoadingFallback name="Recommendations" />
+        {/* <LoadingFallback name="Recommendations" />*/}
       </section>
     </div>
   );
